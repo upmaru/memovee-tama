b5689e30a9b911102aad5774c88e63f63ee80f55
Zack Siri <me@zacksiri.dev>
2025-08-10 22:49:34 +0700

    Setup extract properties

--- movie-db.tf
+++ movie-db.tf
@@ -95,6 +95,44 @@ resource "tama_source_limit" "tmdb-api" {
   value       = 40
 }
 
+# TODO replace this with extract-nested-properties module
+# data "tama_class" "movie-credits" {
+#   specification_id = tama_specification.tmdb.id
+#   name             = "movie-credits"
+# }
+
+# resource "tama_chain" "extract-nested-properties" {
+#   space_id = tama_space.movie-db.id
+#   name     = "Extract Nested Properties"
+# }
+
+# resource "tama_modular_thought" "nested-properties-extraction" {
+#   chain_id = tama_chain.extract-nested-properties.id
+#   index    = 0
+#   relation = "extraction"
+
+#   module {
+#     reference = "tama/classes/extraction"
+#     parameters = jsonencode({
+#       types = ["array"]
+#       depth = 1
+#     })
+#   }
+# }
+
+# resource "tama_thought_path" "extract-nested-properties-movie-credits" {
+#   thought_id      = tama_modular_thought.nested-properties-extraction.id
+#   target_class_id = data.tama_class.movie-credits.id
+# }
+
+# resource "tama_node" "handle-movie-credits-nested-extraction" {
+#   space_id = tama_space.movie-db.id
+#   class_id = module.global.schemas["class-proxy-class"].id
+#   chain_id = tama_chain.extract-nested-properties.id
+
+#   type = "explicit"
+# }
+
 data "tama_class" "movie-details" {
   specification_id = tama_specification.tmdb.id
   name             = "movie-details"
