ea68af5f5db237dc819a32620b67b5daddfe1e47
Zack Siri <me@zacksiri.com>
2025-11-30 22:57:33 +0700

    Switch to devstral medium

--- tmdb-person-browsing.tf
+++ tmdb-person-browsing.tf
@@ -39,7 +39,7 @@ module "person-browsing" {
   routing_thought_relation = module.router.routing_thought_relation
   forwarding_relation      = local.forwarding_relation
 
-  tool_call_model_id          = module.mistral.model_ids["codestral-2508"]
+  tool_call_model_id          = module.mistral.model_ids["devstral-medium-latest"]
   tool_call_tool_choice       = "any"
   tool_call_model_temperature = 0.0
   tool_call_model_parameters  = jsonencode({})
